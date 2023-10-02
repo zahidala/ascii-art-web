95c9f5de097d280fe7617f4d78a5faf99b1cee64
Zahid Allaulddin <zahidalaaudin@gmail.com>
2023-10-02 23:04:12 +0300

    boilerplate code for server side

--- main/main.go
+++ main/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
-	ascii "ascii/pkg"
 	"fmt"
-	"os"
+	"html/template"
+	"net/http"
+	"time"
 )
 
-func main() {
-	// Handle args error
-	err := ascii.ArgsErrors()
-	if err {
-		return
-	}
+type Welcome struct {
+	Name string
+	Time string
+}
 
-	// Convert input to art and print result
-	art := ascii.AsciiArtFS(os.Args[1])
-	fmt.Print(art)
+func main() {
+	// // Handle args error
+	// err := ascii.ArgsErrors()
+	// if err {
+	// 	return
+	// }
+
+	// // Convert input to art and print result
+	// art := ascii.AsciiArtFS(os.Args[1])
+	// fmt.Print(art)
+
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("templates/template.html"))
+
+	http.Handle("/static/", // final URL can be anything
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/ascii", func(w http.ResponseWriter, r *http.Request) {
+		// if have name from http request r -> /?name=your-name-here -> name
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name // set struct object to hold name from URL
+		}
+		// if have error
+		if err := templates.ExecuteTemplate(w, "template.html", welcome); err != nil {
+			// show error message
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
